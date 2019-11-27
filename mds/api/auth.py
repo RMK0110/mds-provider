2d35b357c73c3bd96cafbd5052aca6fb8531ed5e
rmk0110 <ryan.kelly.ladot@gmail.com>
2019-11-27 10:12:30 -0800

    Added Bolt authenticator code block and called necessary libraries

--- mds/api/auth.py
+++ mds/api/auth.py
@@ -1,8 +1,11 @@
 """
 Authentication module for MDS API calls.
 """
-
+from mds.api.auth import AuthorizationToken
+import mds
+import mds.providers
 import requests
+import json
 
 
 class AuthorizationToken():
@@ -75,6 +78,40 @@ class OAuthClientCredentials(AuthorizationToken):
             hasattr(provider, "scope")
         ])
 
+class BoltClientCredentials(AuthorizationToken):
+    """
+    Represents an authenticated session via the Bolt authentication scheme, documented at:
+    <no URL>
+    Currently, your config needs:
+    * email
+    * password
+    * mds_api_url
+    * token_url
+    """
+    def __init__(self, provider):
+        """
+        Acquires the provider token for Bolt before establishing a session.
+        """
+        payload = {
+            "email": provider.email,
+            "password": provider.password
+        }
+        r = requests.post(provider.token_url, params=payload)
+        provider.token = r.json()["token"]
+
+        AuthorizationToken.__init__(self, provider)
+
+    @classmethod
+    def can_auth(cls, provider):
+        """
+        Returns True if this auth type can be used for the provider.
+        """
+        return all([
+            provider.provider_name.lower() == "bolt",
+            hasattr(provider, "email"),
+            hasattr(provider, "password"),
+            hasattr(provider, "token_url")
+        ])
 
 class SpinClientCredentials(AuthorizationToken):
     """
